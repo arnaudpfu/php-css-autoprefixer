044d522e70872917b8fefaa05f7be2979bb2b95a
arnaudpfu <arnaudpfundstein.pro@gmail.com>
2022-05-14 13:23:56 +0200

    Add methods to add new css value to the support attributes of the class

--- class-css-autoprefixer.php
+++ class-css-autoprefixer.php
@@ -9,11 +9,15 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 
 	/**
 	 * Enable to autoprefix CSS
-	 *
 	 */
 	class CSS_Autoprefixer {
 
-		const FULL_SUPPORT = array(
+		/**
+		 * Default unprefixed CSS properties.
+		 *
+		 * @var array
+		 */
+		private $full_support = array(
 			'row-gap',
 			'background-color',
 			'width',
@@ -48,7 +52,12 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 			'stroke-dashoffset',
 		);
 
-		const WEBKIT_SUPPORT = array(
+		/**
+		 * CSS properties webkit prefixed.
+		 *
+		 * @var array
+		 */
+		private $webkit_support = array(
 			'transition-duration',
 			'transition-delay',
 			'transition-timing-function',
@@ -62,20 +71,80 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 			'transform-origin',
 		);
 
-		const O_SUPPORT = array(
+		/**
+		 * CSS properties o prefixed.
+		 *
+		 * @var array
+		 */
+		private $o_support = array(
 			'transition-duration',
 			'transition-delay',
 			'transition-timing-function',
 		);
 
-		const MOZ_SUPPORT = array(
+		/**
+		 * CSS properties moz prefixed.
+		 *
+		 * @var array
+		 */
+		private $moz_support = array(
 			'column-gap',
 		);
 
-		const MS_SUPPORT = array(
+		/**
+		 * CSS properties ms prefixed.
+		 *
+		 * @var array
+		 */
+		private $ms_support = array(
 			'transform-origin',
 			'flex-direction',
 		);
+
+		/**
+		 * Get all_support.
+		 *
+		 * @return array The all_support properties.
+		 */
+		public function get_all_support() {
+			return $this->webkit_support;
+		}
+
+		/**
+		 * Get webkit_support.
+		 *
+		 * @return array The webkit_support properties.
+		 */
+		public function get_webkit_support() {
+			return $this->webkit_support;
+		}
+
+		/**
+		 * Get o_support.
+		 *
+		 * @return array The o_support properties.
+		 */
+		public function get_o_support() {
+			return $this->webkit_support;
+		}
+
+		/**
+		 * Get moz_support.
+		 *
+		 * @return array The moz_support properties.
+		 */
+		public function get_moz_support() {
+			return $this->webkit_support;
+		}
+
+		/**
+		 * Get ms_support.
+		 *
+		 * @return array The ms_support properties.
+		 */
+		public function get_ms_support() {
+			return $this->webkit_support;
+		}
 
 		/**
 		 * Compile CSS
@@ -83,16 +152,55 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param string $unprefixed_css unprefixed CSS code.
 		 * @return string minified and prefixed CSS
 		 */
-		public static function compile( $unprefixed_css ) {
+		public function compile( $unprefixed_css ) {
 
 			$prefixed_css = '';
 
-			$parsed_css   = self::parse( $unprefixed_css );
-			$parsed_css   = self::add_prefix( $parsed_css );
-			$prefixed_css = self::gather_css( $parsed_css );
+			$parsed_css   = $this->parse( $unprefixed_css );
+			$parsed_css   = $this->add_prefix( $parsed_css );
+			$prefixed_css = $this->gather_css( $parsed_css );
 
 			return $prefixed_css;
+		}
 
+		/**
+		 * Add css properties to the webkit_support array
+		 *
+		 * @param array $properties An array of CSS properties in kebab case.
+		 * @return void
+		 */
+		public function add_webkit_support( $properties ) {
+			$this->webkit_support = $this->array_push_all_if_not_inside( $this->webkit_support, $properties );
+		}
+
+		/**
+		 * Add css properties to the o_support array
+		 *
+		 * @param array $properties An array of CSS properties in kebab case.
+		 * @return void
+		 */
+		public function add_o_support( $properties ) {
+			$this->o_support = $this->array_push_all_if_not_inside( $this->o_support, $properties );
+		}
+
+		/**
+		 * Add css properties to the moz_support array
+		 *
+		 * @param array $properties An array of CSS properties in kebab case.
+		 * @return void
+		 */
+		public function add_moz_support( $properties ) {
+			$this->moz_support = $this->array_push_all_if_not_inside( $this->moz_support, $properties );
+		}
+
+		/**
+		 * Add css properties to the ms_support array
+		 *
+		 * @param array $properties An array of CSS properties in kebab case.
+		 * @return void
+		 */
+		public function add_ms_support( $properties ) {
+			$this->ms_support = $this->array_push_all_if_not_inside( $this->ms_support, $properties );
 		}
 
 		/**
@@ -101,13 +209,13 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param array $parsed_css array of CSS_Section.
 		 * @return string minified and prefixed CSS
 		 */
-		public static function gather_css( $parsed_css ) {
+		private function gather_css( $parsed_css ) {
 
 			$gathered_css = '';
 
 			foreach ( $parsed_css as $css_section ) {
 
-				$gathered_css .= self::gather_css_section( $css_section );
+				$gathered_css .= $this->gather_css_section( $css_section );
 			}
 
 			return $gathered_css;
@@ -119,7 +227,7 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param CSS_Section $css_section prefixed CSS.
 		 * @return string minified and prefixed CSS
 		 */
-		public static function gather_css_section( $css_section ) {
+		private function gather_css_section( $css_section ) {
 
 			$gathered_section = null === $css_section->selector ? '' : $css_section->selector . '{';
 
@@ -128,7 +236,7 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 
 				foreach ( $css_keyframes as $css_keyframe ) {
 					$gathered_section .= $css_keyframe['position'] . '{';
-					$gathered_section .= self::gather_css_properties( $css_keyframe['properties'] );
+					$gathered_section .= $this->gather_css_properties( $css_keyframe['properties'] );
 					$gathered_section .= '}';
 				}
 				$gathered_section .= '}';
@@ -143,7 +251,7 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 					}
 				}
 			} else {
-				$gathered_section .= self::gather_css_properties( $css_section->content );
+				$gathered_section .= $this->gather_css_properties( $css_section->content );
 				$gathered_section .= null === $css_section->selector ? '' : '}';
 			}
 
@@ -156,7 +264,7 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param array $css_properties prefixed CSS.
 		 * @return string minified and prefixed CSS
 		 */
-		public static function gather_css_properties( $css_properties ) {
+		private function gather_css_properties( $css_properties ) {
 			$gathered_properties = '';
 
 			foreach ( $css_properties as $prefixed_properties ) {
@@ -174,7 +282,7 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param array $css_sections array of CSS_Section.
 		 * @return array prefixed CSS_Section
 		 */
-		public static function add_prefix( $css_sections ) {
+		private function add_prefix( $css_sections ) {
 
 			$prefixed_css = array();
 
@@ -184,13 +292,13 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 					$css_keyframes      = $css_section->content;
 
 					foreach ( $css_keyframes as $css_keyframe ) {
-						$css_keyframe['properties'] = self::prefix_properties( $css_keyframe['properties'] );
+						$css_keyframe['properties'] = $this->prefix_properties( $css_keyframe['properties'] );
 						array_push( $prefixed_keyframes, $css_keyframe );
 					}
 					$css_section->content           = $prefixed_keyframes;
 					$css_section->position_prefixes = array( '-webkit-', '' );
 				} else {
-					$css_section->content = self::prefix_properties( $css_section->content );
+					$css_section->content = $this->prefix_properties( $css_section->content );
 				}
 				array_push( $prefixed_css, $css_section );
 			}
@@ -204,12 +312,12 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param array $properties parsed CSS properties.
 		 * @return array prefixed CSS properties
 		 */
-		public static function prefix_properties( $properties ) {
+		private function prefix_properties( $properties ) {
 
 			$prefixed_properties = array();
 
 			foreach ( $properties as $property ) {
-				$prefixed_property = self::prefix_property( $property );
+				$prefixed_property = $this->prefix_property( $property );
 				array_push( $prefixed_properties, $prefixed_property );
 			}
 
@@ -222,7 +330,7 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param array $property parsed CSS property.
 		 * @return array prefixed CSS property
 		 */
-		public static function prefix_property( $property ) {
+		private function prefix_property( $property ) {
 
 			$prefixed_property = array();
 
@@ -230,17 +338,17 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 
 			array_push( $prefixed_property['prefixed'], $property );
 
-			if ( ! in_array( $property['property'], self::FULL_SUPPORT, true ) ) {
+			if ( ! in_array( $property['property'], $this->full_support, true ) ) {
 				// webkit.
-				$prefixed_property['prefixed'] = self::check_browser_support( self::WEBKIT_SUPPORT, $prefixed_property['prefixed'], '-webkit-' );
+				$prefixed_property['prefixed'] = $this->check_browser_support( $this->webkit_support, $prefixed_property['prefixed'], '-webkit-' );
 				// opera.
-				$prefixed_property['prefixed'] = self::check_browser_support( self::O_SUPPORT, $prefixed_property['prefixed'], '-o-' );
+				$prefixed_property['prefixed'] = $this->check_browser_support( $this->o_support, $prefixed_property['prefixed'], '-o-' );
 				// moz.
-				$prefixed_property['prefixed'] = self::check_browser_support( self::MOZ_SUPPORT, $prefixed_property['prefixed'], '-moz-' );
+				$prefixed_property['prefixed'] = $this->check_browser_support( $this->moz_support, $prefixed_property['prefixed'], '-moz-' );
 				// ms.
-				$prefixed_property['prefixed'] = self::check_browser_support( self::MS_SUPPORT, $prefixed_property['prefixed'], '-ms-' );
+				$prefixed_property['prefixed'] = $this->check_browser_support( $this->ms_support, $prefixed_property['prefixed'], '-ms-' );
 
-				$prefixed_property['prefixed'] = self::check_specific_support( $prefixed_property['prefixed'] );
+				$prefixed_property['prefixed'] = $this->check_specific_support( $prefixed_property['prefixed'] );
 			}
 
 			return $prefixed_property;
@@ -252,9 +360,9 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param array $prefixed_css_properties already added prefixed properties.
 		 * @return array the new $prefixed_css_properties
 		 */
-		public static function check_specific_support( $prefixed_css_properties ) {
+		private function check_specific_support( $prefixed_css_properties ) {
 
-			$native_property = self::get_native_property( $prefixed_css_properties );
+			$native_property = $this->get_native_property( $prefixed_css_properties );
 
 			switch ( $native_property['property'] ) {
 				case 'align-items':
@@ -262,14 +370,14 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 						$prefixed_css_properties,
 						array(
 							'property' => '-webkit-box-align',
-							'value'    => self::get_flex_prefixed_value( $native_property['value'] ),
+							'value'    => $this->get_flex_prefixed_value( $native_property['value'] ),
 						)
 					);
 					array_push(
 						$prefixed_css_properties,
 						array(
 							'property' => '-ms-flex-align',
-							'value'    => self::get_flex_prefixed_value( $native_property['value'] ),
+							'value'    => $this->get_flex_prefixed_value( $native_property['value'] ),
 						)
 					);
 					break;
@@ -279,14 +387,14 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 						$prefixed_css_properties,
 						array(
 							'property' => '-webkit-box-pack',
-							'value'    => self::get_flex_prefixed_value( $native_property['value'] ),
+							'value'    => $this->get_flex_prefixed_value( $native_property['value'] ),
 						)
 					);
 					array_push(
 						$prefixed_css_properties,
 						array(
 							'property' => '-ms-flex-pack',
-							'value'    => self::get_flex_prefixed_value( $native_property['value'] ),
+							'value'    => $this->get_flex_prefixed_value( $native_property['value'] ),
 						)
 					);
 					break;
@@ -308,7 +416,7 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 						$prefixed_css_properties,
 						array(
 							'property' => '-webkit-box-orient',
-							'value'    => self::get_flex_prefixed_value( $native_property['value'] ),
+							'value'    => $this->get_flex_prefixed_value( $native_property['value'] ),
 						)
 					);
 					array_push(
@@ -332,9 +440,9 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param string $prefix added.
 		 * @return array the new $prefixed_css_properties
 		 */
-		public static function check_browser_support( $linked_properties, $prefixed_css_properties, $prefix ) {
+		private function check_browser_support( $linked_properties, $prefixed_css_properties, $prefix ) {
 
-			$native_property = self::get_native_property( $prefixed_css_properties );
+			$native_property = $this->get_native_property( $prefixed_css_properties );
 
 			if ( in_array( $native_property['property'], $linked_properties, true ) ) {
 				$prefixed_property = array(
@@ -355,7 +463,7 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param array $prefixed_css_properties all the prefixed properties.
 		 * @return array
 		 */
-		public static function get_native_property( $prefixed_css_properties ) {
+		private function get_native_property( $prefixed_css_properties ) {
 			return $prefixed_css_properties[0];
 		}
 
@@ -365,7 +473,7 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param string $value css.
 		 * @return string
 		 */
-		public static function get_flex_prefixed_value( $value ) {
+		private function get_flex_prefixed_value( $value ) {
 
 			switch ( $value ) {
 				case 'flex-start':
@@ -391,13 +499,13 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param string $unprefixed_css unprefixed and unminified CSS.
 		 * @return array
 		 */
-		public static function parse( $unprefixed_css ) {
+		private function parse( $unprefixed_css ) {
 
 			$parsed_css = array();
 
-			if ( self::has_selector( $unprefixed_css ) ) {
+			if ( $this->has_selector( $unprefixed_css ) ) {
 
-				$css_text_sections = self::split_into_css_text_section( $unprefixed_css );
+				$css_text_sections = $this->split_into_css_text_section( $unprefixed_css );
 
 				foreach ( $css_text_sections as $css_text_section ) {
 
@@ -417,7 +525,7 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param string $css CSS code.
 		 * @return boolean true if CSS has at least one selector
 		 */
-		public static function has_selector( $css ) {
+		private function has_selector( $css ) {
 			return strpos( $css, '}' ) !== false || strpos( $css, '@keyframes ' ) !== false;
 		}
 
@@ -427,10 +535,26 @@ if ( ! class_exists( 'CSS_Autoprefixer' ) ) {
 		 * @param string $css code.
 		 * @return array
 		 */
-		public static function split_into_css_text_section( $css ) {
+		private function split_into_css_text_section( $css ) {
 			$css = preg_replace( '/(?<=\}|\{|,|\n|\r)\s*from\s*(?=\{|,)/', '0%', $css );
 			$css = preg_replace( '/(?<=\}|\{|,|\n|\r)\s*to\s*(?=\{|,)/', '100%', $css );
 			return preg_split( '/(?<=\})(?=[a-zA-Z|#|\s|\.|@]+)/', $css );
+		}
+
+		/**
+		 * Push all elements of an array inside another one if elements aren't inside.
+		 *
+		 * @param array $targeted_array The input array.
+		 * @param array $pushed_array The pushed array.
+		 * @return array The input array with its new value.
+		 */
+		private function array_push_all_if_not_inside( $targeted_array, $pushed_array ) {
+			foreach ( $pushed_array as $pushed_value ) {
+				if ( ! in_array( $pushed_value, $targeted_array, true ) ) {
+					array_push( $targeted_array, $pushed_value );
+				}
+			}
+			return $targeted_array;
 		}
 	}
 }
